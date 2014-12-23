0f3089d7c7d7beda403c956dbc7bea2b6062c7fd
Svitlana <chicagobusinessintelligence1@gmail.com>
2014-12-23 14:39:11 -0600

    Initial Commit

--- app/src/main.js
+++ app/src/main.js
@@ -1,18 +1,15 @@
 /* globals define */
-define(function(require, exports, module) {
+define(function (require, exports, module) {
     'use strict';
     // import dependencies
     var Engine = require('famous/core/Engine');
-    var Modifier = require('famous/core/Modifier');
     var Transform = require('famous/core/Transform');
-    var ImageSurface = require('famous/surfaces/ImageSurface');
 
     // create the main context
     var mainContext = Engine.createContext();
 
     var AppView = require('views/AppView');
     require('famous/inputs/FastClick');
-
 
     var appView = new AppView();
 
--- app/views/HeaderView.js
+++ app/views/HeaderView.js
@@ -1,73 +1,72 @@
 define(function (require, exports, module) {
-	var Surface = require('famous/core/Surface');
-	var Modifier = require('famous/core/Modifier');
-	var Transform = require('famous/core/Transform');
-	var View = require('famous/core/View');
+    var Surface = require('famous/core/Surface');
+    var Modifier = require('famous/core/Modifier');
+    var Transform = require('famous/core/Transform');
+    var View = require('famous/core/View');
 
-	function HeaderView() {
-		View.apply(this, arguments);
+    function HeaderView() {
+        View.apply(this, arguments);
 
-		_createHeader.call(this);
-		_setListeners.call(this);
-	}
+        _createHeader.call(this);
+        _setListeners.call(this);
+    }
 
-	HeaderView.prototype = Object.create(View.prototype);
-	HeaderView.prototype.constructor = HeaderView;
+    HeaderView.prototype = Object.create(View.prototype);
+    HeaderView.prototype.constructor = HeaderView;
 
-	function _createHeader() {
-		var backgroundSurface = new Surface({
-			size: [undefined, undefined],
-			properties: {
-				// coloring pink, but background should not be visible anyway when viewed at device screen size
-				backgroundColor: 'rgb(236,73,137)'
-			}
-		});
+    function _createHeader() {
+        var backgroundSurface = new Surface({
+            size: [undefined, undefined],
+            properties: {
+                // coloring pink, but background should not be visible anyway when viewed at device screen size
+                backgroundColor: 'rgb(236,73,137)'
+            }
+        });
 
-		this.hamburgerSurface = new Surface({
-			size: [53, undefined],
-			content: '<img width="53" src="img/hamburger.png"/>'
-		});
+        this.hamburgerSurface = new Surface({
+            size: [53, undefined],
+            content: '<img width="53" src="img/hamburger.png"/>'
+        });
 
-		this.titleSurface = new Surface({
-			size: [267, undefined],
-			content: 'SVET Media Group222',
-			properties: {
-				fontSize: '22px',
-				color: "white",
-				lineHeight: "50px",
-				fontWeight: '700'
-			}
-		});
+        this.titleSurface = new Surface({
+            size: [267, undefined],
+            content: 'SVET Media Group',
+            properties: {
+                fontSize: '22px',
+                color: "white",
+                lineHeight: "50px",
+                fontWeight: '700'
+            }
+        });
 
-		this.hamburgerModifier = new Modifier({
-			transform: Transform.translate(0, 0, 1)
-		});
+        this.hamburgerModifier = new Modifier({
+            transform: Transform.translate(0, 0, 1)
+        });
 
-		this.titleModifier = new Modifier({
-			align: [0.5,0],
-			origin: [1, 0]
-		});
+        this.titleModifier = new Modifier({
+            origin: [1, 0]
+        });
 
-		this._add(this.hamburgerModifier).add(this.hamburgerSurface);
-		this._add(this.titleModifier).add(this.titleSurface);
+        this._add(this.hamburgerModifier).add(this.hamburgerSurface);
+        this._add(this.titleModifier).add(this.titleSurface);
 
-		this._add(backgroundSurface);
-	}
+        this._add(backgroundSurface);
+    }
 
-	function _setListeners() {
-		this.hamburgerSurface.on('touchstart', function () {
-			this.hamburgerModifier.setOpacity(0.5);
-		}.bind(this));
+    function _setListeners() {
+        this.hamburgerSurface.on('touchstart', function () {
+            this.hamburgerModifier.setOpacity(0.5);
+        }.bind(this));
 
-		this.hamburgerSurface.on('mousedown', function () {
-			this.hamburgerModifier.setOpacity(0.5);
-		}.bind(this));
+        this.hamburgerSurface.on('mousedown', function () {
+            this.hamburgerModifier.setOpacity(0.5);
+        }.bind(this));
 
-		this.hamburgerSurface.on('click', function () {
-			this.hamburgerModifier.setOpacity(1);
-			this._eventOutput.emit('menuToggle');
-		}.bind(this));
-	}
+        this.hamburgerSurface.on('click', function () {
+            this.hamburgerModifier.setOpacity(1);
+            this._eventOutput.emit('menuToggle');
+        }.bind(this));
+    }
 
-	module.exports = HeaderView;
+    module.exports = HeaderView;
 });
