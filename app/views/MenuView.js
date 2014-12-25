7332bb0c766fc3477a684378f002e79db8125383
Svitlana <chicagobusinessintelligence1@gmail.com>
2014-12-25 12:30:23 -0600

    Navigation Menu Design #3

--- app/views/MenuView.js
+++ app/views/MenuView.js
@@ -1,99 +1,99 @@
 define(function (require, exports, module) {
-	var Surface = require('famous/core/Surface');
-	var Modifier = require('famous/core/Modifier');
-	var Transform = require('famous/core/Transform');
-	var View = require('famous/core/View');
-	var Timer = require('famous/utilities/Timer');
+    var Surface = require('famous/core/Surface');
+    var Modifier = require('famous/core/Modifier');
+    var Transform = require('famous/core/Transform');
+    var View = require('famous/core/View');
+    var Timer = require('famous/utilities/Timer');
 
-	var NavigationView = require('./NavigationView');
+    var NavigationView = require('./NavigationView');
 
-	function MenuView() {
-		View.apply(this, arguments);
+    function MenuView() {
+        View.apply(this, arguments);
 
-		_createBacking.call(this);
-		_createNavigationViews.call(this);
-	}
+        _createBacking.call(this);
+        _createNavigationViews.call(this);
+    }
 
-	MenuView.prototype = Object.create(View.prototype);
-	MenuView.prototype.constructor = MenuView;
+    MenuView.prototype = Object.create(View.prototype);
+    MenuView.prototype.constructor = MenuView;
 
-	MenuView.prototype.resetNavItems = function () {
-		for (var i = 0; i < this.navModifiers.length; i++) {
-			var initX = -this.options.navWidth / 4;
-			var initY = this.options.topOffset + this.options.navItemOffset * i + this.options.navHeight * 2;
+    MenuView.prototype.resetNavItems = function () {
+        for (var i = 0; i < this.navModifiers.length; i++) {
+            var initX = -this.options.navWidth / 4;
+            var initY = this.options.topOffset + this.options.navItemOffset * i + this.options.navHeight * 2;
 
-			this.navModifiers[i].setOpacity(0.0);
-			this.navModifiers[i].setTransform(Transform.translate(initX, initY, 0));
-		}
-	};
+            this.navModifiers[i].setOpacity(0.0);
+            this.navModifiers[i].setTransform(Transform.translate(initX, initY, 0));
+        }
+    };
 
-	MenuView.prototype.animateNavItems = function () {
-		this.resetNavItems();
+    MenuView.prototype.animateNavItems = function () {
+        this.resetNavItems();
 
-		for (var i = 0; i < this.navModifiers.length; i++) {
-			// use Timer.setTimeout instead of window.setTimeout
-			// Time can be found in famous/utilities
+        for (var i = 0; i < this.navModifiers.length; i++) {
+            // use Timer.setTimeout instead of window.setTimeout
+            // Time can be found in famous/utilities
 
-			Timer.setTimeout(function (i) {
-				var yOffset = this.options.topOffset + this.options.navItemOffset * i;
+            Timer.setTimeout(function (i) {
+                var yOffset = this.options.topOffset + this.options.navItemOffset * i;
 
-				this.navModifiers[i].setOpacity(1, {duration: this.options.duration, curve: 'easeOut'});
-				this.navModifiers[i].setTransform(
-					Transform.translate(0, yOffset, 0),
-					{duration: this.options.duration, curve: 'easeOut'});
-			}.bind(this, i), i * this.options.staggerDelay);
-		}
-	};
+                this.navModifiers[i].setOpacity(1, {duration: this.options.duration, curve: 'easeOut'});
+                this.navModifiers[i].setTransform(
+                    Transform.translate(0, yOffset, 0),
+                    {duration: this.options.duration, curve: 'easeOut'});
+            }.bind(this, i), i * this.options.staggerDelay);
+        }
+    };
 
-	MenuView.DEFAULT_OPTIONS = {
-		navWidth: 191,
-		navHeight: 81,
-		topOffset: 10,
-		navItemOffset: 90,
-		duration: 400,
-		staggerDelay: 35
-	};
+    MenuView.DEFAULT_OPTIONS = {
+        navWidth: 191,
+        navHeight: 81,
+        topOffset: 10,
+        navItemOffset: 90,
+        duration: 400,
+        staggerDelay: 35
+    };
 
-	function _createBacking() {
-		var backSurface = new Surface({
-			size: [this.options.width, this.options.height],
-			properties: {
-				backgroundColor: '#595153'
-			}
-		});
+    function _createBacking() {
+        var backSurface = new Surface({
+            size: [this.options.width, this.options.height],
+            properties: {
+                backgroundColor: '#595153'
+            }
+        });
 
-		this._add(backSurface);
-	}
+        this._add(backSurface);
+    }
 
-	function _createNavigationViews() {
-		this.navModifiers = [];
+    function _createNavigationViews() {
+        this.navModifiers = [];
 
-		var navData = [
-			{iconUrl: 'img/nav-icons/about-us.png'},
-			{iconUrl: 'img/nav-icons/clients.png'},
-			{iconUrl: 'img/nav-icons/entertainment.png'},
-			{iconUrl: 'img/nav-icons/sports.png'},
-			{iconUrl: 'img/nav-icons/dribble.png'},
-			{iconUrl: 'img/nav-icons/reddit.png'},
-		];
+        var navData = [
+            {iconUrl: 'img/nav-icons/home.png'},
+            {iconUrl: 'img/nav-icons/about-us.png'},
+            {iconUrl: 'img/nav-icons/demographics.png'},
+            {iconUrl: 'img/nav-icons/clients.png'},
+            {iconUrl: 'img/nav-icons/contact-us.png'},
+            {iconUrl: 'img/nav-icons/radio.png'},
+        ];
 
-		for (var i = 0; i < navData.length; i++) {
-			var navView = new NavigationView({
-				width: this.options.navWidth,
-				height: this.options.navHeight,
-				iconUrl: navData[i].iconUrl
-			});
+        for (var i = 0; i < navData.length; i++) {
+            var navView = new NavigationView({
+                width: this.options.navWidth,
+                height: this.options.navHeight,
+                iconUrl: navData[i].iconUrl
+            });
 
-			var yOffset = this.options.topOffset + this.options.navItemOffset * i;
+            var yOffset = this.options.topOffset + this.options.navItemOffset * i;
 
-			var navModifier = new Modifier({
-				transform: Transform.translate(0, yOffset, 0)
-			});
+            var navModifier = new Modifier({
+                transform: Transform.translate(0, yOffset, 0)
+            });
 
-			this.navModifiers.push(navModifier);
-			this._add(navModifier).add(navView);
-		}
-	}
+            this.navModifiers.push(navModifier);
+            this._add(navModifier).add(navView);
+        }
+    }
 
-	module.exports = MenuView;
+    module.exports = MenuView;
 });
