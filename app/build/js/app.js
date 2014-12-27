c3f58960ee724dfdada36bce07487f831d5401c5
Svitlana <chicagobusinessintelligence1@gmail.com>
2014-12-27 17:59:23 -0600

    Views

--- app/build/js/app.js
+++ app/build/js/app.js
@@ -5757,8 +5757,8 @@ define('views/MenuView',['require','exports','module','famous/core/Surface','fam
             {iconUrl: 'img/nav-icons/about-us.png'},
             {iconUrl: 'img/nav-icons/demographics.png'},
             {iconUrl: 'img/nav-icons/clients.png'},
-            {iconUrl: 'img/nav-icons/contact-us.png'},
             {iconUrl: 'img/nav-icons/radio.png'},
+            {iconUrl: 'img/nav-icons/contact-us.png'},
         ];
 
         for (var i = 0; i < navData.length; i++) {
@@ -9941,35 +9941,77 @@ define('views/PageView',['require','exports','module','famous/core/Surface','fam
             footerSize: 50
         });
 
+        /*Header*/
         this.header = new HeaderView();
         this.header.pipe(this);
+
+        /*Content*/
         this.contents = [];
-        this.content = new ScrollView() ;
-        //this.content.pipe(this);
+        this.content = new ScrollView();
 
-        this.contentTop = new Surface({
+        this.contentHome = new Surface({
             size: [undefined, undefined],
-            content:'I Love you :-). Vse bude ok!',
+            content: '<h2><em>SVET Media Group</em> is the Midwest’s first and oldest publishing and advertising company serving the Russian, Ukrainian and Lithuanian communities since 1990.</h2>',
             properties: {
-                color:'red',
-                backgroundColor: '#A8FFFF'
+                color: 'black',
+                fontSize: '16px',
+                backgroundColor: '#FFA47F'
             }
         });
-
-        this.contentBottom = new Surface({
+        this.contentAbout = new Surface({
             size: [undefined, undefined],
-            content:'Bottom',
+            content: '<h2>SVET International publishing house</h2>' +
+            '<p>From the viewpoint of our partners SVET International Publishing House is a typical "company with the past", which basic philosophy is hinged upon well-taken conservatism, weighed approach and clear calculations. It was not for nothing that all previous outside convulsions and crises bypassed our publishing house. Our meticulous attitude towards entering into deals is completely justified by strict performance of undertaken liabilities and flawless financial stability. </p>',
             properties: {
-                backgroundColor: '#FAFBCB'
+                backgroundColor: '#FAC883'
             }
         });
-        this.contents.push(this.contentTop);
-        this.contents.push(this.contentBottom);
+        this.contentDemographics = new Surface({
+            size: [undefined, undefined],
+            content: '<h2>Demographics</h2>' +
+            '<p>The Russian - American population in the United States is estimated at nearly 2.9 million people</p>',
+            properties: {
+                backgroundColor: '#95C79B'
+            }
+        });
+        this.contentClients = new Surface({
+            size: [undefined, undefined],
+            content: '<h2>Demographics</h2>' +
+            '<p>The Russian - American population in the United States is estimated at nearly 2.9 million people</p>',
+            properties: {
+                backgroundColor: '#FFB1AC'
+            }
+        });
+        this.contentRadio = new Surface({
+            size: [undefined, undefined],
+            content: '<h2>Radio Program “OSA”</h2>' +
+            '<p>Sunday morning talk show with Alex Etman airs every Sunday on 1240 AM radio from 11:00 a.m. to 1:00 p.m.</p>',
+            properties: {
+                backgroundColor: '#83BFE3'
+            }
+        });
+        this.contentContact = new Surface({
+            size: [undefined, undefined],
+            content: '<h2>Contact Us</h2>' +
+            '<p>Sunday morning talk show with Alex Etman airs every Sunday on 1240 AM radio from 11:00 a.m. to 1:00 p.m.</p>',
+            properties: {
+                backgroundColor: '#998EBA'
+            }
+        });
+        this.contents.push(this.contentHome);
+        this.contents.push(this.contentAbout);
+        this.contents.push(this.contentDemographics);
+        this.contents.push(this.contentClients);
+        this.contents.push(this.contentRadio);
+        this.contents.push(this.contentContact);
         this.content.sequenceFrom(this.contents);
 
-        this.contentTop.pipe(this.content);
-        this.contentBottom.pipe(this.content);
-        /* =Grid*/
+        this.contentHome.pipe(this.content);
+        this.contentAbout.pipe(this.content);
+        this.contentDemographics.pipe(this.content);
+        this.contentClients.pipe(this.content);
+        this.contentRadio.pipe(this.content);
+        this.contentContact.pipe(this.content);
 
         /* =Footer*/
         this.footers = [];
@@ -10006,8 +10048,8 @@ define('views/PageView',['require','exports','module','famous/core/Surface','fam
         this.footers.push(this.footerRight);
         this.footer.sequenceFrom(this.footers);
 
-        this.layout.header.add(this.header);
         this.layout.content.add(this.content);
+        this.layout.header.add(this.header);
         this.layout.footer.add(this.footer);
 
         this._eventInput.pipe(this._eventOutput);
@@ -10140,8 +10182,6 @@ define('main',['require', 'famous/core/Engine', 'views/AppView'], function (requ
     var appView = new AppView();
 
     mainContext.add(appView);
-
-
 });
 
 define("main", function(){});
@@ -10154,6 +10194,4 @@ define(['require', 'famous/core/Engine', 'views/AppView'], function (require, En
     var appView = new AppView();
 
     mainContext.add(appView);
-
-
 });
