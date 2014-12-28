a3facf9a9bca4c98426ee0c29501e8febb4132b7
Svitlana <chicagobusinessintelligence1@gmail.com>
2014-12-28 08:53:23 -0600

    ddd

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/app/dist'));
+
+var dev = 'app/build/',
+    dist = 'app/dist/';
+
+
+app.use(express.static(path.join(__dirname, dist)));
+
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
