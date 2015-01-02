7818fcdd619617b465a32645aecefd95d2cafcd0
Svitlana <chicagobusinessintelligence1@gmail.com>
2015-01-02 10:11:57 -0600

    bootstrap styles

---
+++ app/src/views/DemographicView.js
@@ -0,0 +1,115 @@
+define(function (require, exports, module) {
+    var Surface = require('famous/core/Surface');
+    var Modifier = require('famous/core/Modifier');
+    var Transform = require('famous/core/Transform');
+    var View = require('famous/core/View');
+    var GridLayout = require('famous/views/GridLayout');
+    var Timer = require('famous/utilities/Timer');
+
+    var EventHandler = require('famous/core/EventHandler');
+    var NavigationView = require('./NavigationView');
+
+
+    function DemographicView() {
+        var that = this;
+        View.apply(this, arguments);
+
+        this.eventInput = new EventHandler();
+        this.eventOutput = new EventHandler();
+        EventHandler.setInputHandler(this, this.eventInput);
+        EventHandler.setOutputHandler(this, this.eventOutput);
+
+        var demographicsPart1 = require('text!jade/demographicsPage.html');
+        var svg = _createSvg();
+        var demographicsPart2 = require('text!jade/demographicsPart2.html');
+
+        this.contentPart1= new Surface({
+            size: [undefined, undefined],
+            content: demographicsPart1,
+            properties: {
+                backgroundColor: '#FFFAE2'
+            }
+        });
+        this.contentPart2= new Surface({
+            size: [undefined, undefined],
+            content: svg,
+            properties: {
+                backgroundColor: '#FFFAE2'
+            }
+        });
+        this.contentPart3= new Surface({
+            size: [undefined, undefined],
+            content: demographicsPart2,
+            properties: {
+                backgroundColor: '#FFFAE2'
+            }
+        });
+        var demographContent = [];
+        demographContent.push(this.contentPart1);
+        demographContent.push(this.contentPart2);
+        demographContent.push(this.contentPart3);
+
+        var grid = new GridLayout({
+            direction: 1,
+            dimensions: [1, 3]
+        });
+        grid.sequenceFrom(demographContent);
+
+        this.add(grid);
+
+        this.contentPart1.pipe(grid);
+        this.contentPart2.pipe(grid);
+        this.contentPart3.pipe(grid);
+
+        grid.pipe(this);
+    }
+
+    function _createSvg() {
+        var svg = document.createElementNS(d3.ns.prefix.svg, 'svg');
+        var w = 200;
+        var h = 200;
+        var r = h / 2;
+        var color = d3.scale.category20c();
+
+        var data = [{"label": "Russian Speaking Population", "value": 20},
+            {"label": "Hispanic", "value": 50},
+            {"label": "Others", "value": 30}];
+
+        var vis = d3.select(svg).data([data]).attr("width", w).attr("height", h).append("svg:g").attr("transform", "translate(" + r + "," + r + ")");
+        var pie = d3.layout.pie().value(function (d) {
+            return d.value;
+        });
+
+        var arc = d3.svg.arc().outerRadius(r);
+
+        var arcs = vis.selectAll("g.slice").data(pie).enter().append("svg:g").attr("class", "slice");
+        arcs.append("svg:path")
+            .attr("fill", function (d, i) {
+                return color(i);
+            })
+            .attr("d", function (d) {
+                // log the result of the arc generator to show how cool it is :)
+                console.log(arc(d));
+                return arc(d);
+            });
+
+        arcs.append("svg:text").attr("transform", function (d) {
+            d.innerRadius = 0;
+            d.outerRadius = r;
+            return "translate(" + arc.centroid(d) + ")";
+        }).attr("text-anchor", "middle").text(function (d, i) {
+                return data[i].label;
+            }
+        );
+        return svg;
+    }
+
+    DemographicView.prototype = Object.create(View.prototype);
+    DemographicView.prototype.constructor = DemographicView;
+
+
+    DemographicView.DEFAULT_OPTIONS = {};
+
+
+    module.exports = DemographicView;
+});
--- app/src/views/HomeScroll.js
+++ app/src/views/HomeScroll.js
@@ -7,10 +7,8 @@ define(function (require, exports, module) {
     var ViewSequence = require('famous/core/ViewSequence');
 
     var StateModifier = require('famous/modifiers/StateModifier');
-    var Scrollview = require('famous/views/ScrollContainer');
-    var barChartView = require('views/barChartView');
+    var DemographicView = require('views/DemographicView');
     var d3 = require('d3/d3');
-    console.log(nv);
 
     var EventHandler = require('famous/core/EventHandler');
 
@@ -53,12 +51,11 @@ define(function (require, exports, module) {
 
         var homePage = require('text!jade/homePage.html');
         var aboutUsPage = require('text!jade/aboutUsPage.html');
-        var demographicsPage = require('text!jade/demographicsPage.html');
+        this.demographicsPage = new DemographicView();
         var clientsPage = require('text!jade/clientsPage.html');
         var radioPage = require('text!jade/radioPage.html');
         var contactUsPage = require('text!jade/contactUsPage.html');
 
-        var test = d3.text(demographicsPage);
 
         this.contents = [];
 
@@ -77,53 +74,8 @@ define(function (require, exports, module) {
                 backgroundColor: '#E6FFEF'
             }
         });
-        var svg = document.createElementNS(d3.ns.prefix.svg, 'svg');
-        var w = 400;
-        var h = 400;
-        var r = h / 2;
-        var color = d3.scale.category20c();
 
-        var data = [{"label": "Russian Speaking Population", "value": 20},
-            {"label": "Hispanic", "value": 50},
-            {"label": "Others", "value": 30}];
 
-        var vis = d3.select(svg).data([data]).attr("width", w).attr("height", h).append("svg:g").attr("transform", "translate(" + r + "," + r + ")");
-        var pie = d3.layout.pie().value(function (d) {
-            return d.value;
-        });
-
-// declare an arc generator function
-        var arc = d3.svg.arc().outerRadius(r);
-
-// select paths, use arc generator to draw
-        var arcs = vis.selectAll("g.slice").data(pie).enter().append("svg:g").attr("class", "slice");
-        arcs.append("svg:path")
-            .attr("fill", function (d, i) {
-                return color(i);
-            })
-            .attr("d", function (d) {
-                // log the result of the arc generator to show how cool it is :)
-                console.log(arc(d));
-                return arc(d);
-            });
-
-// add the text
-        arcs.append("svg:text").attr("transform", function (d) {
-            d.innerRadius = 0;
-            d.outerRadius = r;
-            return "translate(" + arc.centroid(d) + ")";
-        }).attr("text-anchor", "middle").text(function (d, i) {
-                return data[i].label;
-            }
-        );
-
-        this.contentDemographics = new Surface({
-            size: [undefined, undefined],
-            content: svg,
-            properties: {
-                backgroundColor: '#FFFAE2'
-            }
-        });
         this.contentClients = new Surface({
             size: [undefined, undefined],
             content: clientsPage,
@@ -148,7 +100,7 @@ define(function (require, exports, module) {
 
         this.contentHome.pipe(genericSync);
         this.contentAbout.pipe(genericSync);
-        this.contentDemographics.pipe(genericSync);
+        this.demographicsPage.pipe(genericSync);
         this.contentClients.pipe(genericSync);
         this.contentRadio.pipe(genericSync);
         this.contentContact.pipe(genericSync);
@@ -172,7 +124,7 @@ define(function (require, exports, module) {
 
         this.contents.push(this.contentHome);
         this.contents.push(this.contentAbout);
-        this.contents.push(this.contentDemographics);
+        this.contents.push(this.demographicsPage);
         this.contents.push(this.contentClients);
         this.contents.push(this.contentRadio);
         this.contents.push(this.contentContact);
