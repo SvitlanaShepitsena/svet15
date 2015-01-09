80d6bc656e1de7b3d7c8fb5e17f44cad54725942
Svitlana <chicagobusinessintelligence1@gmail.com>
2015-01-09 12:16:54 -0600

    Common View Design

--- app/src/views/pages/common/CommonView.js
+++ app/src/views/pages/common/CommonView.js
@@ -9,67 +9,88 @@ define(function (require, exports, module) {
     Transitionable.registerMethod('spring', SpringTransition);
 
 
-    function CommonView() {
+    function SlideView() {
+
         View.apply(this, arguments);
+
         this.rootModifier = new StateModifier({
-            align: this.options.align,
-            origin: this.options.origin,
+            align: [0.5, 0.0],
+            origin: [0.5, 0.0],
             size: this.options.size
         });
 
         this.mainNode = this.add(this.rootModifier);
 
         _createBackground.call(this);
-        _createViewContent.call(this);
+        _createView.call(this);
     }
 
-    CommonView.prototype = Object.create(View.prototype);
-    CommonView.prototype.constructor = CommonView;
+    SlideView.prototype = Object.create(View.prototype);
+    SlideView.prototype.constructor = SlideView;
 
-    CommonView.DEFAULT_OPTIONS = {
-        angle: -0.5,
-        align: [0.5, 0.5],
-        origin: [0.5, 0.5],
-        bg: '#ffffff',
-        boxShadow: '0 2px 4px -2px rgba(0, 0, 0, 0.5)',
-        width: window.innerWidth,
-        height: window.innerHeight
+    SlideView.DEFAULT_OPTIONS = {
+        size: [400, 550],
+        filmBorder: 15,
+        photoBorder: 3,
+        angle: -0.5
     };
 
     function _createBackground() {
-        this.background = new Surface({
+        var background = new Surface({
             properties: {
                 backgroundColor: this.options.bg,
-                boxShadow: this.options.boxShadow,
+                boxShadow: '0 10px 20px -5px rgba(0, 0, 0, 0.5)',
                 cursor: 'pointer'
             }
         });
 
-        this.background.pipe(this._eventOutput);
-        this.mainNode.add(this.background);
+        this.mainNode.add(background);
 
-        this.background.on('click', function () {
+        background.on('click', function () {
             this._eventOutput.emit('click');
         }.bind(this));
     }
 
 
-    function _createViewContent() {
-        this.contentModifier = new StateModifier({
-            align: this.options.align,
-            origin: this.options.origin,
-            size: [this.options.width * .9, this.options.height * .8],
-            transform: Transform.translate(0, 0, 2)
-        });
+    function _createView() {
 
-        this.commonViewContent = new Surface({
+        var view = new Surface({
+            size: this.options.size,
             content: this.options.content,
             properties: {
+                zIndex: 2,
                 pointerEvents: 'none'
+
             }
         });
-        this.mainNode.add(this.contentModifier).add(this.commonViewContent);
+
+        this.photoModifier = new StateModifier({
+            origin: [0.5, 0],
+            align: [0.5, 0],
+            transform: Transform.translate(20, this.options.filmBorder + this.options.photoBorder, 0.1)
+        });
+
+        this.mainNode.add(this.photoModifier).add(view);
     }
 
-    module.exports = CommonView;
+    SlideView.prototype.fadeIn = function () {
+        this.photoModifier.setOpacity(1, {duration: 1500, curve: 'easeIn'});
+        this.shake();
+    };
+
+    SlideView.prototype.shake = function () {
+        this.rootModifier.halt();
+
+        this.rootModifier.setTransform(
+            Transform.rotateX(this.options.angle),
+            {duration: 200, curve: 'easeOut'}
+        );
+
+        this.rootModifier.setTransform(
+            Transform.identity,
+            {method: 'spring', period: 600, dampingRatio: 0.15}
+        );
+    };
+
+    module.exports = SlideView;
 });
