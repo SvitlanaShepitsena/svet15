a35253d86acb6011f55d576656ec01e4ea9d63ad
Andriy <svitlana.shepitsena@chicagobusinessintelligence.net>
2015-01-12 14:31:36 -0600

    Desktop Scroll

--- app/src/views/desktop/content/DeskScroll.js
+++ app/src/views/desktop/content/DeskScroll.js
@@ -5,30 +5,37 @@ define(function (require, exports, module) {
     var Modifier = require("famous/core/Modifier");
     var ScrollContainer = require('famous/views/ScrollContainer');
 
-    var AboutUsDesk = require('dviews/content/dpages/AboutUsDesk');
     var HomeDesk = require('dviews/content/dpages/HomeDesk');
+    var AboutUsDesk = require('dviews/content/dpages/AboutUsDesk');
+
 
     function DeskScroll() {
         ScrollContainer.apply(this, arguments);
         this.scrollContent = [];
+        _fillContent.call(this);
 
+        this.scrollview.sequenceFrom(this.scrollContent);
+
+        _pipe.call(this);
+    }
+
+    function _pipe() {
+        for (var i = 0; i < this.scrollContent.length; i++) {
+            var surface = this.scrollContent[i];
+            surface.pipe(this.scrollview);
+        }
+
+    }
+
+
+    function _fillContent() {
         this.homeDesk = new HomeDesk();
         this.aboutUsDesk = new AboutUsDesk();
 
 
         this.scrollContent.push(this.homeDesk);
         this.scrollContent.push(this.aboutUsDesk);
-
-        this.scrollview.sequenceFrom(this.scrollContent);
-        _push.call(this);
     }
-        function _push() {
-            for (var i = 0; i < this.scrollContent.length; i++) {
-                var surface = this.scrollContent[i];
-                surface.pipe(this.scrollview);
-            }
-
-        }
 
 
     DeskScroll.prototype = Object.create(ScrollContainer.prototype);
