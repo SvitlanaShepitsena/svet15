f0ea03996217c121fa7a0cf1373b83e3d883bfb8
Andriy <svitlana.shepitsena@chicagobusinessintelligence.net>
2015-01-15 12:13:22 -0600

    Map. Import

--- app/src/views/cell/content/home/MapsCell.js
+++ app/src/views/cell/content/home/MapsCell.js
@@ -5,6 +5,7 @@ define(function (require, exports, module) {
     var Modifier = require("famous/core/Modifier");
     var MapView = require('fmaps/MapView');
     var Transitionable = require('famous/transitions/Transitionable');
+    var MapModifier = require('famous-map/MapModifier');
     var Easing = require('famous/transitions/Easing');
 
 
@@ -12,16 +13,36 @@ define(function (require, exports, module) {
         View.apply(this, arguments);
         _init.call(this);
         _map.call(this);
+        _modifier.call(this);
+    }
+
+    function _modifier() {
+
+        this.surface = new Surface({
+            size: [50, 50],
+            properties: {
+                backgroundColor: 'white'
+            }
+        });
+        this.modifier = new Modifier({
+            align: [0, 0],
+            origin: [0.5, 0.5]
+        });
+        this.mapModifier = new MapModifier({
+            mapView: this.mapView,
+            position: {lat: 51.4484855, lng: 5.451478}
+        });
+        this.add(mapModifier).add(modifier).add(surface);
     }
 
     function _map() {
-        var northChicago = {lat: 41.850033, lng: -87.6500523};
-        var northChicago2 = {lat: 41.95, lng: -87.6500523};
+        var northChicagoStart = {lat: 41.850033, lng: -87.6500523};
+        var northChicagoEnd = {lat: 41.95, lng: -87.6500523};
         var mapView = new MapView({
             type: MapView.MapType.GOOGLEMAPS,
             mapOptions: {
-                zoom: 10,
-                center: northChicago,
+                zoom: 9,
+                center: northChicagoStart,
                 mapTypeId: google.maps.MapTypeId.ROADMAP
             }
         });
@@ -29,9 +50,8 @@ define(function (require, exports, module) {
 
         mapView.on('load', function () {
             mapView.setPosition(
-                northChicago2,
+                northChicagoEnd,
                 {duration: 5000, curve: Easing.outBack}
-
             );
         }.bind(this));
 
