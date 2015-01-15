511ed7212c72d8128bec8b32880c01587c98fb04
Svitlana <chicagobusinessintelligence1@gmail.com>
2015-01-15 12:15:08 -0600

    test

---
+++ app/lib/famous-bkimagesurface/BkImageSurface.js
@@ -0,0 +1,206 @@
+/**
+ * This Source Code is licensed under the MIT license. If a copy of the
+ * MIT-license was not distributed with this file, You can obtain one at:
+ * http://opensource.org/licenses/mit-license.html.
+ *
+ * @author: Hein Rutjes (IjzerenHein)
+ * @license MIT
+ * @copyright Gloey Apps, 2014
+ */
+
+/*global define*/
+
+/**
+ * BkImageSurface adds support for sizing-strategies such as AspectFit and AspectFill for displaying images with famo.us.
+ * It uses a 'div' with a background-image rather than a 'img' tag.
+ *
+ * Can be used as a drop-in replacement for ImageSurface, in case the the size of the div is not derived
+ * from the image.
+ *
+ * @module
+ */
+define(function(require, exports, module) {
+    'use strict';
+
+    // import dependencies
+    var Surface = require('famous/core/Surface');
+
+    /**
+     * @enum
+     * @alias module:BkImageSurface.SizeMode
+     */
+    var SizeMode = {
+        AUTO: 'auto',
+        FILL: '100% 100%',
+        ASPECTFILL: 'cover',
+        ASPECTFIT: 'contain'
+    };
+
+    /**
+     * @enum
+     * @alias module:BkImageSurface.PositionMode
+     */
+    var PositionMode = {
+        CENTER: 'center center',
+        LEFT: 'left center',
+        RIGHT: 'right center',
+        TOP: 'center top',
+        BOTTOM: 'center bottom',
+        TOPLEFT: 'left top',
+        TOPRIGHT: 'right top',
+        BOTTOMLEFT: 'left bottom',
+        BOTTOMRIGHT: 'right bottom'
+    };
+
+    /**
+     * @enum
+     * @alias module:BkImageSurface.RepeatMode
+     */
+    var RepeatMode = {
+        NONE: 'no-repeat',
+        VERTICAL: 'repeat-x',
+        HORIZONTAL: 'repeat-y',
+        BOTH: 'repeat'
+    };
+
+    /**
+     * @class
+     * @param {Object} options Options.
+     * @param {String} [options.content] Image-url.
+     * @param {SizeMode|String} [options.sizeMode] Size-mode to use.
+     * @param {PositionMode|String} [options.positionMode] Position-mode to use.
+     * @param {RepeatMode|String} [options.repeatMode] Repeat-mode to use.
+     * @alias module:BkImageSurface
+     */
+    var BkImageSurface = function(options) {
+        Surface.apply(this, arguments);
+        this.content = undefined;
+        this._imageUrl = options ? options.content : undefined;
+        this._sizeMode = (options && options.sizeMode) ? options.sizeMode : SizeMode.FILL;
+        this._positionMode = (options && options.positionMode) ? options.positionMode : PositionMode.CENTER;
+        this._repeatMode = (options && options.repeatMode) ? options.repeatMode : RepeatMode.NONE;
+
+        this._updateProperties();
+    };
+    BkImageSurface.prototype = Object.create(Surface.prototype);
+    BkImageSurface.prototype.constructor = BkImageSurface;
+    BkImageSurface.prototype.elementType = 'div';
+    BkImageSurface.prototype.elementClass = 'famous-surface';
+    BkImageSurface.SizeMode = SizeMode;
+    BkImageSurface.PositionMode = PositionMode;
+    BkImageSurface.RepeatMode = RepeatMode;
+
+    /**
+     * Update the css-styles on the div.
+     *
+     * @private
+     */
+    BkImageSurface.prototype._updateProperties = function() {
+        var props = this.getProperties();
+        if (this._imageUrl) {
+            var imageUrl = this._imageUrl;
+            // url encode '(' and ')'
+            if ((imageUrl.indexOf('(') >= 0) || (imageUrl.indexOf(')') >= 0)) {
+                imageUrl = imageUrl.split('(').join('%28');
+                imageUrl = imageUrl.split(')').join('%29');
+            }
+            props.backgroundImage = 'url(' + imageUrl + ')';
+        } else {
+            props.backgroundImage = '';
+        }
+        props.backgroundSize = this._sizeMode;
+        props.backgroundPosition = this._positionMode;
+        props.backgroundRepeat = this._repeatMode;
+        this.setProperties(props);
+    };
+
+    /**
+     * @param {String} imageUrl Image-url, when set will cause re-rendering
+     */
+    BkImageSurface.prototype.setContent = function(imageUrl) {
+        this._imageUrl = imageUrl;
+        this._updateProperties();
+    };
+
+    /**
+     * @return {String} Image-url
+     */
+    BkImageSurface.prototype.getContent = function() {
+        return this._imageUrl;
+    };
+
+    /**
+     * @param {SizeMode|String} sizeMode Sizing-mode, when set will cause re-rendering
+     */
+    BkImageSurface.prototype.setSizeMode = function(sizeMode) {
+        this._sizeMode = sizeMode;
+        this._updateProperties();
+    };
+
+    /**
+     * @return {SizeMode|String} Size-mode
+     */
+    BkImageSurface.prototype.getSizeMode = function() {
+        return this._sizeMode;
+    };
+
+    /**
+     * @param {PositionMode|String} positionMode Position-mode, when set will cause re-rendering
+     */
+    BkImageSurface.prototype.setPositionMode = function(positionMode) {
+        this._positionMode = positionMode;
+        this._updateProperties();
+    };
+
+    /**
+     * @return {RepeatMode|String} Position-mode
+     */
+    BkImageSurface.prototype.getPositionMode = function() {
+        return this._positionMode;
+    };
+
+    /**
+     * @param {RepeatMode|String} repeatMode Repeat-mode, when set will cause re-rendering
+     */
+    BkImageSurface.prototype.setRepeatMode = function(repeatMode) {
+        this._repeatMode = repeatMode;
+        this._updateProperties();
+    };
+
+    /**
+     * @return {RepeatMode|String} Repeat-mode
+     */
+    BkImageSurface.prototype.getRepeatMode = function() {
+        return this._repeatMode;
+    };
+
+    /**
+     * Place the document element that this component manages into the document.
+     *
+     * NOTE: deploy and recall were added because famo.us removed the background-image
+     * after the surface was removed/re-added from the DOM.
+     *
+     * @private
+     * @param {Node} target document parent of this container
+     */
+    BkImageSurface.prototype.deploy = function deploy(target) {
+        if (this._imageUrl) {
+            target.style.backgroundImage = 'url(' + this._imageUrl + ')';
+        }
+    };
+
+    /**
+     * Remove this component and contained content from the document
+     *
+     * NOTE: deploy and recall were added because famo.us removed the background-image
+     * after the surface was removed/re-added from the DOM.
+     *
+     * @private
+     * @param {Node} target node to which the component was deployed
+     */
+    BkImageSurface.prototype.recall = function recall(target) {
+        target.style.backgroundImage = '';
+    };
+
+    module.exports = BkImageSurface;
+});
---
+++ app/lib/famous-bkimagesurface/README.md
@@ -0,0 +1,127 @@
+famous-bkimagesurface
+==========
+
+BkImageSurface adds support for sizing-strategies such as AspectFit and AspectFill for displaying images with famo.us.
+It uses a 'div' with a background-image rather than a 'img' tag.
+
+Can be used as a drop-in replacement for ImageSurface, *in case the the size of the div is not derived from the image*.
+
+## Demo
+
+[View the demo here](https://rawgit.com/IjzerenHein/famous-imageview/master/examples/demo/index.html)
+
+	
+## Getting started
+
+Install using bower or npm:
+
+    bower install famous-bkimagesurface
+
+    npm install famous-bkimagesurface
+
+Example of how to create a BkImageSurface:
+
+```javascript
+var BkImageSurface = require('famous-bkimagesurface');
+
+var imageSurface = new BkImageSurface({
+    content: 'myimage.png',
+    sizeMode: BkImageSurface.SizeMode.ASPECTFILL
+});
+this.add(imageSurface);
+```
+
+all possible modes:
+
+```javascript
+var imageSurface = new BkImageSurface({
+    content: 'myimage.png',
+    sizeMode: BkImageSurface.SizeMode.ASPECTFILL,
+    positionMode: BkImageSurface.PositionMode.TOP,
+    repeatMode: BkImageSurface.RepeatMode.NONE
+});
+this.add(imageSurface);
+```
+
+using css-style values directly:
+
+```javascript
+var imageSurface = new BkImageSurface({
+    content: 'myimage.png',
+    sizeMode: '100px 50%',
+    positionMode: 'center center',
+    repeatMode: 'repeat-x'
+});
+this.add(imageSurface);
+```
+
+using the setter-functions:
+
+```javascript
+var imageSurface = new BkImageSurface();
+imageSurface.setContent('http://www.myimage.png');
+imageSurface.setSizeMode(BkImageSurface.SizeMode.ASPECTFIT);
+imageSurface.setPositionMode(BkImageSurface.PositionMode.RIGHT);
+imageSurface.setRepeatMode(BkImageSurface.RepeatMode.VERTICAL);
+```
+
+## Documentation
+
+The size-, position- and repeat- modes correspond directly to the following css-styles:
+
+|Mode|CSS-style|
+|----|---------|
+|SizeMode|`background-size`|
+|PositionMode|`background-position`|
+|RepeatMode|`background-repeat`|
+
+According to your liking you can choose to use either the Mode-definitions or CSS-styles directly.
+
+### Size-modes
+
+|Mode|Value|Description|
+|-----|---------|-----------|
+|SizeMode.AUTO|auto|Keeps the original image dimensions.|
+|SizeMode.FILL **(default)**|100% 100%|Fills the image to the size of the div.|
+|SizeMode.ASPECTFILL|cover|Fills the div with the image while keeping correct image aspect ratio (crops if neccesary).|
+|SizeMode.ASPECTFIT|contain|Fits the whole image in the div while keeping correct image aspect ratio.|
+
+### Position-modes
+
+|Mode|Value|Description|
+|----|-----|-----------|
+|PositionMode.CENTER **(default)**|center center|Centers the image in the div.|
+|PositionMode.LEFT|left center|Left aligns the image in the div.|
+|PositionMode.RIGHT|right center|Right aligns the image in the div.|
+|PositionMode.TOP|center top|Top aligns the image in the div.|
+|PositionMode.BOTTOM|center bottom|Bottom aligns the image in the div.|
+|PositionMode.TOPLEFT|left top|Aligns the image in the top-left corner of the div.|
+|PositionMode.TOPRIGHT|right top|Aligns the image in the top-right corner of the div.|
+|PositionMode.BOTTOMLEFT|left bottom|Aligns the image in the bottom-left corner of the div.|
+|PositionMode.BOTTOMRIGHT|right bottom|Aligns the image in the bottom-right corner of the div.|
+
+### Repeat-modes
+
+|Mode|Value|Description|
+|-----|---------|-----------|
+|RepeatMode.NONE **(default)**|no-repeat|No image-repeat.|
+|RepeatMode.HORIZONTAL|repeat-y|Image is repeated horizontally.|
+|RepeatMode.VERTICAL|repeat-x|Image is repeated vertically.|
+|RepeatMode.BOTH|repeat|Image is repeated both horizontally and vertically.|
+
+### API reference
+
+|Class|Description|
+|---|---|
+|[BkImageSurface](docs/BkImageSurface.md)|Background image surface class.|
+
+## Contribute
+
+Feel free to contribute to this project in any way. The easiest way to support this project is by giving it a star.
+
+## Contact
+- 	@IjzerenHein
+- 	http://www.gloey.nl
+- 	hrutjes@gmail.com
+
+© 2014 - Hein Rutjes
