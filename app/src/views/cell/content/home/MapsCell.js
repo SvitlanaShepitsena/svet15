4087ce67872b0e02f04a7e4a90b3df916c698a36
Svitlana <chicagobusinessintelligence1@gmail.com>
2015-01-18 16:33:48 -0600

    Skokie Region Schema Drawing

--- app/src/views/cell/content/home/MapsCell.js
+++ app/src/views/cell/content/home/MapsCell.js
@@ -77,12 +77,33 @@ define(function (require, exports, module) {
             this.gMap = this.mapView.getMap();
 
             var skokieCoordinates = [
-                new google.maps.LatLng(42.063568,-87.771006),
-                new google.maps.LatLng(42.052479,-87.709551),
-                new google.maps.LatLng(42.012571,-87.710238),
-                new google.maps.LatLng(42.008361,-87.767401),
-                new google.maps.LatLng(42.040760, -87.764376),
-                new google.maps.LatLng(42.063568,-87.771006)
+                new google.maps.LatLng(42.065100, -87.765800),
+                new google.maps.LatLng(42.065100, -87.727563),
+                new google.maps.LatLng(42.055828, -87.727563),
+                new google.maps.LatLng(42.055828, -87.715718),
+                new google.maps.LatLng(42.052131, -87.708852),
+                new google.maps.LatLng(42.012095, -87.709367),
+                new google.maps.LatLng(42.012095, -87.723614),
+                new google.maps.LatLng(42.015156, -87.724129),
+                new google.maps.LatLng(42.015156, -87.750565),
+                new google.maps.LatLng(42.004569, -87.750909),
+                new google.maps.LatLng(42.004952, -87.762238),
+                new google.maps.LatLng(42.008396, -87.767560),
+                new google.maps.LatLng(42.019237, -87.767216),
+                new google.maps.LatLng(42.019237, -87.777173),
+                new google.maps.LatLng(42.026506, -87.780949),
+                new google.maps.LatLng(42.026506, -87.767216),
+                new google.maps.LatLng(42.023063, -87.767388),
+                new google.maps.LatLng(42.022808, -87.762582),
+                new google.maps.LatLng(42.033264, -87.762067),
+                new google.maps.LatLng(42.037089, -87.766873),
+                new google.maps.LatLng(42.039129, -87.764298),
+                new google.maps.LatLng(42.062583, -87.763955),
+                new google.maps.LatLng(42.062583, -87.769963),
+                new google.maps.LatLng(42.063730, -87.770821),
+                new google.maps.LatLng(42.063730, -87.765964),
+
+                new google.maps.LatLng(42.065100, -87.765800)
             ];
 
             var skokieLayer = new google.maps.Polygon({
