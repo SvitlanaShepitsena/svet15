a08479d2c3cf0451e336666de96add10fa851dc1
Svitlana <chicagobusinessintelligence1@gmail.com>
2015-01-19 12:47:21 -0600

    Glencoe Grove Region Schema Drawing

--- app/src/views/cell/content/home/MapsCell.js
+++ app/src/views/cell/content/home/MapsCell.js
@@ -164,6 +164,68 @@ define(function (require, exports, module) {
             });
             deerfieldLayer.setMap(this.gMap);
 
+            //
+            /*Northbrook*/
+
+            var northbrookCoordinates = [
+                /*Top Border with Northbrook*/
+                new google.maps.LatLng(42.152516, -87.795999),
+                new google.maps.LatLng(42.152887, -87.833732),
+                new google.maps.LatLng(42.146269, -87.830985),
+                new google.maps.LatLng(42.146015, -87.833389),
+                new google.maps.LatLng(42.150088, -87.843002),
+                new google.maps.LatLng(42.150342, -87.872012),
+                new google.maps.LatLng(42.153269, -87.883857),
+                /*Bottom Border wiht Glenview*/
+                new google.maps.LatLng(42.153152, -87.892301),
+                new google.maps.LatLng(42.145516, -87.892129),
+                new google.maps.LatLng(42.145516, -87.881829),
+                new google.maps.LatLng(42.142970, -87.885263),
+                new google.maps.LatLng(42.138515, -87.887838),
+                new google.maps.LatLng(42.129477, -87.887323),
+                new google.maps.LatLng(42.123875, -87.885434),
+                new google.maps.LatLng(42.105793, -87.866208),
+                new google.maps.LatLng(42.105284, -87.799089),
+                new google.maps.LatLng(42.110887, -87.802350),
+                new google.maps.LatLng(42.118018, -87.800290),
+                new google.maps.LatLng(42.117509, -87.785356),
+                new google.maps.LatLng(42.119928, -87.780034),
+                new google.maps.LatLng(42.152516, -87.795999)
+            ];
+            var northbrookLayer = new google.maps.Polygon({
+                paths: northbrookCoordinates,
+                strokeColor: 'red',
+                strokeOpacity: 0.8,
+                strokeWeight: 2,
+                fillColor: 'orange',
+                fillOpacity: 0.35
+            });
+            northbrookLayer.setMap(this.gMap);
+            //
+            /*Glencoe*/
+
+            var glencoeCoordinates = [
+                /*Top Border with Northbrook*/
+                new google.maps.LatLng(42.152516, -87.795999),
+                new google.maps.LatLng(42.119928, -87.780034),
+                new google.maps.LatLng(42.116280, -87.775601),
+                new google.maps.LatLng(42.119973, -87.775601),
+                new google.maps.LatLng(42.119591, -87.741268),
+                new google.maps.LatLng(42.128503, -87.741612),
+                new google.maps.LatLng(42.152433, -87.759293),
+
+                new google.maps.LatLng(42.152516, -87.795999)
+            ];
+            var glencoeLayer = new google.maps.Polygon({
+                paths: glencoeCoordinates,
+                strokeColor: 'blue',
+                strokeOpacity: 0.8,
+                strokeWeight: 2,
+                fillColor: 'coral',
+                fillOpacity: 0.35
+            });
+            glencoeLayer.setMap(this.gMap);
+
 
             /*Skokie*/
             var skokieCoordinates = [
