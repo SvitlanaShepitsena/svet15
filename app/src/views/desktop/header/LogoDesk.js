3d7c01047990db6f3030204894424bae5d33571a
Andriy <svitlana.shepitsena@chicagobusinessintelligence.net>
2015-01-21 13:02:36 -0600

    Raphael Animation

--- app/src/views/desktop/header/LogoDesk.js
+++ app/src/views/desktop/header/LogoDesk.js
@@ -8,26 +8,41 @@ define(function (require, exports, module) {
 
     function LogoDesk() {
         View.apply(this, arguments);
-        this.centerModifier = new Modifier({
-            align: [0.5, 0.5],
-            origin: [0.5, 0.5],
-            transform: Transform.translate(0, 0, 0)
-        });
-        this.rootNode = this.add(this.centerModifier);
-        _rmgText.call(this);
+        _init.call(this);
         _svetText.call(this);
+        _rmgText.call(this);
         _logoSvg.call(this);
     }
 
     function _logoSvg() {
-        var paper = Raphael(10, 50, 320, 200);
-        // Creates circle at x = 50, y = 40, with radius 10
-        var circle = paper.circle(50, 40, 10);
-// Sets the fill attribute of the circle to red (#f00)
-        circle.attr("fill", "#f00");
 
-// Sets the stroke attribute of the circle to white
-        circle.attr("stroke", "#fff");
+        var div = document.createElement('div');
+
+        var paper = Raphael(div, 256, 100);
+        var line2 = paper.path("M5 50");
+        var line3 = paper.path("M125 10");
+        var line4 = paper.path("M246 50");
+
+        var line = paper.path("M50 50").animate({path: "M50 50 L 5 50"}, 500, function () {
+            line2.animate({path: "M5 50 L125 10"}, 500, function () {
+                line3.animate({path: "M125 10 L246 50"}, 500, function () {
+                    line4.animate({path: "M246 50 L200 50"}, 500, function () {
+                    })
+                })
+            });
+        });
+        line.attr("stroke", "red");
+        line.attr("stroke-width", "5px");
+
+        line2.attr("stroke", "red");
+        line2.attr("stroke-width", "5px");
+
+        line3.attr("stroke", "red");
+        line3.attr("stroke-width", "5px");
+
+        line4.attr("stroke", "red");
+        line4.attr("stroke-width", "5px");
+
         this.logoSvgMod = new Modifier({
             size: [undefined, undefined],
             align: [0, 0],
@@ -36,7 +51,7 @@ define(function (require, exports, module) {
         });
         this.logoSvgSurf = new Surface({
             size: [undefined, undefined],
-            content: 'paper.circle',
+            content: div,
             classes: [],
             properties: {
                 color: 'white',
@@ -44,14 +59,15 @@ define(function (require, exports, module) {
             }
         });
         this.rootNode.add(this.logoSvgMod).add(this.logoSvgSurf);
+
     }
 
     function _svetText() {
         this.svetTextMod = new Modifier({
-            size: [undefined, 30],
+            size: [undefined, 100],
             align: [0, 0],
             origin: [0, 0],
-            transform: Transform.translate(0, 0, 0)
+            transform: Transform.translate(0, 20, 0)
         });
         this.svetTextSurf = new Surface({
             size: [undefined, undefined],
@@ -59,33 +75,55 @@ define(function (require, exports, module) {
             classes: [],
             properties: {
                 color: 'white',
-                textAlign: 'center',
-                backgroundColor: '#FA5C4F'
+                textAlign: 'center'
             }
         });
         this.rootNode.add(this.svetTextMod).add(this.svetTextSurf);
     }
 
     function _rmgText() {
+
+        this.mediaSurfMod = new Modifier({
+            size: [undefined, 100],
+            align: [0, 0],
+            origin: [0, 0],
+            transform: Transform.translate(0, 40, 0)
+        });
         this.mediaSurface = new Surface({
-            size: [undefined, 50],
+            size: [undefined, 100],
             content: 'Russian Media Group',
             classes: [],
             properties: {
                 color: 'white',
-                textAlign: 'center',
-                backgroundColor: '#FA5C4F'
+                textAlign: 'center'
             }
         });
 
-        var imageSurface = new ImageSurface();
-        imageSurface.setContent('img/logo.png');
-        //this.svgSurface = new Surface({
-        //    size: [undefined, undefined],
-        //    content: mysvg
-        //});
-        this.rootNode.add(this.mediaSurface);
+
+        this.rootNode.add(this.mediaSurfMod).add(this.mediaSurface);
     }
+
+    function _init() {
+        this.bg = new Surface({
+            size: [undefined, undefined],
+            content: '',
+            classes: [],
+            properties: {
+                color: 'white',
+                textAlign: 'center',
+                backgroundColor: 'blueviolet'
+            }
+        });
+        this.centerModifier = new Modifier({
+            align: [0.5, 0.5],
+            origin: [0.5, 0.5],
+            transform: Transform.translate(0, 0, 0)
+        });
+
+        this.rootNode = this.add(this.centerModifier);
+        this.rootNode.add(this.bg);
+    }
+
 
     LogoDesk.prototype = Object.create(View.prototype);
     LogoDesk.prototype.constructor = LogoDesk;
