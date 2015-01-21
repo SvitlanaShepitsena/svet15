a2c8fb15e8746391714d9669caea58216e6a9311
Svitlana <chicagobusinessintelligence1@gmail.com>
2015-01-21 13:03:20 -0600

    Desktop Header Animation

--- app/src/views/desktop/header/LogoDesk.js
+++ app/src/views/desktop/header/LogoDesk.js
@@ -5,6 +5,8 @@ define(function (require, exports, module) {
     var Transform = require('famous/core/Transform');
     var Modifier = require("famous/core/Modifier");
 
+    var logosvg = require('text!dviews/jade/svgtraining.html');
+
 
     function LogoDesk() {
         View.apply(this, arguments);
@@ -14,20 +16,12 @@ define(function (require, exports, module) {
             transform: Transform.translate(0, 0, 0)
         });
         this.rootNode = this.add(this.centerModifier);
+        _logoSvg.call(this);
         _rmgText.call(this);
         _svetText.call(this);
-        _logoSvg.call(this);
     }
 
     function _logoSvg() {
-        var paper = Raphael(10, 50, 320, 200);
-        // Creates circle at x = 50, y = 40, with radius 10
-        var circle = paper.circle(50, 40, 10);
-// Sets the fill attribute of the circle to red (#f00)
-        circle.attr("fill", "#f00");
-
-// Sets the stroke attribute of the circle to white
-        circle.attr("stroke", "#fff");
         this.logoSvgMod = new Modifier({
             size: [undefined, undefined],
             align: [0, 0],
@@ -36,45 +30,53 @@ define(function (require, exports, module) {
         });
         this.logoSvgSurf = new Surface({
             size: [undefined, undefined],
-            content: 'paper.circle',
+            content: logosvg,
             classes: [],
             properties: {
                 color: 'white',
+                backgroundColor: 'grey',
                 textAlign: 'center'
             }
         });
         this.rootNode.add(this.logoSvgMod).add(this.logoSvgSurf);
+
     }
 
     function _svetText() {
         this.svetTextMod = new Modifier({
             size: [undefined, 30],
-            align: [0, 0],
-            origin: [0, 0],
+            align: [0.5, 1],
+            origin: [0.5, 1],
             transform: Transform.translate(0, 0, 0)
         });
         this.svetTextSurf = new Surface({
             size: [undefined, undefined],
             content: 'SVET',
-            classes: [],
             properties: {
+                fontSize: '30px',
+                fontWeight: 'bold',
                 color: 'white',
-                textAlign: 'center',
-                backgroundColor: '#FA5C4F'
+                textAlign: 'center'
             }
         });
         this.rootNode.add(this.svetTextMod).add(this.svetTextSurf);
     }
 
     function _rmgText() {
-        this.mediaSurface = new Surface({
+        this.mediaMod = new Modifier({
             size: [undefined, 50],
-            content: 'Russian Media Group',
+            align: [0.5, 1],
+            origin: [0.5, 1],
+            transform: Transform.translate(0, 0, 0)
+        });
+        this.mediaSurface = new Surface({
+            size: [undefined, undefined],
+            content: 'RUSSIAN MEDIA GROUP',
             classes: [],
             properties: {
                 color: 'white',
-                textAlign: 'center',
-                backgroundColor: '#FA5C4F'
+                fontSize: '21px',
+                textAlign: 'center'
             }
         });
 
@@ -84,7 +86,7 @@ define(function (require, exports, module) {
         //    size: [undefined, undefined],
         //    content: mysvg
         //});
-        this.rootNode.add(this.mediaSurface);
+        this.rootNode.add(this.mediaMod).add(this.mediaSurface);
     }
 
     LogoDesk.prototype = Object.create(View.prototype);
