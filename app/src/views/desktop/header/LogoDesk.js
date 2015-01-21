eed53d144c611b5e4f4d3d1f09e922f75adbbb61
Svitlana <chicagobusinessintelligence1@gmail.com>
2015-01-21 11:11:48 -0600

    Desktop Header Animation

--- app/src/views/desktop/header/LogoDesk.js
+++ app/src/views/desktop/header/LogoDesk.js
@@ -5,6 +5,7 @@ define(function (require, exports, module) {
     var Transform = require('famous/core/Transform');
     var Modifier = require("famous/core/Modifier");
 
+    var mysvg = require('text!dviews/jade/svgtraining.html');
 
     function LogoDesk() {
         View.apply(this, arguments);
@@ -13,16 +14,19 @@ define(function (require, exports, module) {
             origin: [0.5, 0.5],
             transform: Transform.translate(0, 0, 0)
         });
-        this.rootNode = this.add(this.centerModifier) ;
+        this.rootNode = this.add(this.centerModifier);
         _logo.call(this);
     }
 
     function _logo() {
-        var imageSurface = new ImageSurface();
-        imageSurface.setContent('img/logo.png');
-
-
-        this.rootNode.add(imageSurface);
+        //var imageSurface = new ImageSurface();
+        //imageSurface.setContent('img/logo.png');
+        this.svgSurface = new Surface({
+            size: [undefined, undefined],
+            content: mysvg
+            //content: '<svg width="100" height="100"> <circle cx="50" cy="50" r="40" stroke="red" stroke-width="4" fill="yellow" /> </svg>'
+        });
+        this.rootNode.add(this.svgSurface);
     }
 
     LogoDesk.prototype = Object.create(View.prototype);
