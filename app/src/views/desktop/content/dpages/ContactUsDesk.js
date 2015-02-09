4e7df6a74ffca900afbf6819ee332c05e93af7d9
Svetic <svitlana.shepitsena@chicagobusinessintelligence.net>
2015-02-09 13:05:59 -0600

    Contact Us Map Destination Form

--- app/src/views/desktop/content/dpages/ContactUsDesk.js
+++ app/src/views/desktop/content/dpages/ContactUsDesk.js
@@ -52,7 +52,7 @@ define(function (require, exports, module) {
         this.rootNode.add(this.mapSurface);
 
         _addMap.call(this);
-        _addContent.call(this);
+        //_addContent.call(this);
     }
 
     function _addContent() {
@@ -95,8 +95,12 @@ define(function (require, exports, module) {
 
             var elm = document.getElementById(this.mapId);
 
+            function setTransportType() {
+                console.log('findWhere');
+
+            }
+
             if (elm) {
-                map = this.map;
                 var mapOptions = {
                     center: this.officeCoord,
                     zoom: 12,
@@ -110,6 +114,10 @@ define(function (require, exports, module) {
                 };
 
                 this.map = new google.maps.Map(elm, mapOptions);
+                map = this.map;
+
+                var that = this;
+                that.transportType = google.maps.TravelMode.DRIVING;
 
                 var svetMarker = new google.maps.Marker({
                     position: this.officeCoord,
@@ -128,8 +136,81 @@ define(function (require, exports, module) {
 
                 // Instantiate an info window to hold step text.
                 stepDisplay = new google.maps.InfoWindow();
-                console.log(directionForm);
                 elm.innerHTML = elm.innerHTML + directionForm;
+
+                var markerArray = [];
+                if (navigator.geolocation) {
+                    navigator.geolocation.getCurrentPosition(function (pos) {
+
+                        var geocoder = new google.maps.Geocoder();
+                        var userLatLng = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+                        geocoder.geocode({'latLng': userLatLng}, function (results, status) {
+                            var infowindow = new google.maps.InfoWindow();
+                            if (status == google.maps.GeocoderStatus.OK) {
+                                if (results[0]) {
+                                    map.setZoom(11);
+                                    marker = new google.maps.Marker({
+                                        position: userLatLng,
+                                        map: map
+                                    });
+                                    var address = results[0].formatted_address;
+
+                                    var startDirection = document.getElementById('start');
+                                    startDirection.value = address;
+
+                                    var searchButton = document.getElementById('searchRoute');
+                                    searchButton.onclick = function () {
+                                        calcRoute();
+                                    };
+
+                                    var bus = document.getElementById('byPublic');
+                                    bus.onclick = function () {
+                                        that.transportType = google.maps.TravelMode.TRANSIT;
+                                        calcRoute();
+                                    };
+                                    var car = document.getElementById('byCar');
+                                    car.onclick = function () {
+                                        that.transportType = google.maps.TravelMode.DRIVING;
+                                        calcRoute();
+                                    };
+                                }
+                            } else {
+                                alert("Geocoder failed due to: " + status);
+                            }
+                        });
+                    });
+                } else {
+                    error('not supported');
+                }
+
+                function calcRoute() {
+
+                    for (var i = 0; i < markerArray.length; i++) {
+                        markerArray[i].setMap(null);
+                    }
+
+                    // Retrieve the start and end locations and create
+                    // a DirectionsRequest using WALKING directions.
+
+                    var start = document.getElementById("start").value;
+                    var end = document.getElementById("end").value;
+                    var request = {
+                        origin: start,
+                        destination: end,
+                        travelMode:that.transportType
+                    };
+
+                    // Route the directions and pass the response to a
+                    // function to create markers for each step.
+                    directionsService.route(request, function (response, status) {
+                        if (status == google.maps.DirectionsStatus.OK) {
+                            var warnings = document.getElementById("warnings_panel");
+                            warnings.innerHTML = "" + response.routes[0].warnings + "";
+                            directionsDisplay.setDirections(response);
+                        }
+                    });
+                }
+
 
             }
 
