65b13676dd981c855aeb5a95963e3d49d18d3c55
andriy.shepitsen@chicagobusinessintelligence.net <Ytrewq12>
2015-02-23 14:17:29 -0600

    Zoom Settings in Map

--- app/src/views/cell/content/common/CommonSlideCell.js
+++ app/src/views/cell/content/common/CommonSlideCell.js
@@ -47,15 +47,14 @@ define(function (require, exports, module) {
             align: this.options.align,
             origin: this.options.origin,
             size: [this.options.width * .9, this.options.height * .8],
-            transform: Transform.translate(0, 0, 2)
+            transform: Transform.translate(0, 0, 20)
         });
         this.viewContent = new Surface({
             content: require('text!cviews/jade/' + this.options.folder + '/' + this.options.content + '.html'),
             properties: {
                 textAlign: 'center',
                 marginBottom: '10px',
-                zIndex: 2,
-                pointerEvents: 'none'
+                zIndex: 20
             }
         });
         this.rootNode.add(this.contentModifier).add(this.viewContent);
